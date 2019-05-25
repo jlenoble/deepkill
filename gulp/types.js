b9d750c30c435036b59117de6e3b619de34e2e69
Jason Lenoble <jason.lenoble@gmail.com>
2019-05-25 18:43:03 +0200

    Applied gen wup 0.6.0-5
    This adds types declarations and completes prepublish

---
+++ gulp/types.js
@@ -0,0 +1,20 @@
+import gulp from "gulp";
+import ts from "gulp-typescript";
+
+const srcDir = "src";
+const libDir = "lib";
+const libGlob = ["src/**/*.ts", "src/**/*.js"];
+const tsProject = ts.createProject("tsconfig.json");
+
+const handleTypes = () => {
+  const tsResult = gulp
+    .src(libGlob, {
+      base: srcDir,
+      since: gulp.lastRun(handleTypes)
+    })
+    .pipe(tsProject());
+
+  return tsResult.dts.pipe(gulp.dest(libDir));
+};
+
+gulp.task("types", handleTypes);
